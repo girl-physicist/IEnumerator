ebce0c786c94dc6ac9ff646ee26d7dd59fc87728
Anastasiya <girl-physicist@live.com>
2018-09-28 19:14:36 +0300

    прога1

--- IEnumerator/Program.cs
+++ IEnumerator/Program.cs
@@ -1,15 +1,41 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace IEnumerator
+namespace Enumerator
 {
     class Program
     {
         static void Main(string[] args)
-        {
+        { // необобщенная коллекция ArrayList
+            ArrayList objectList = new ArrayList() { 1, 2, "string", 'c', 2.0f };
+
+            object obj = 45.8;
+
+            objectList.Add(obj);
+            objectList.Add("string2");
+            objectList.RemoveAt(0); // удаление первого элемента
+            foreach (object o in objectList)
+            {
+                Console.WriteLine(o);
+            }
+            Console.WriteLine("Общее число элементов коллекции: {0}", objectList.Count);
+            // обобщенная коллекция List
+            List<string> countries = new List<string>() { "<Беларусь", "США", "Великобритания", "Китай" };
+            countries.Add("Франция");
+            countries.RemoveAt(1); // удаление второго элемента
+            foreach (string s in countries)
+            {
+                Console.WriteLine(s);
+            }
+   
+
+
+            Console.ReadLine();
+            //С помощью свойства Count у коллекций можно посмотреть количество элементов.
         }
     }
 }
